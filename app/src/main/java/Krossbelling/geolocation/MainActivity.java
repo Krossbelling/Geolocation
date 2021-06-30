bc2f64757d2b85113bf9593b6f1eda37772b655d
Krossbelling <Githubbalance@mail.ru>
2021-06-30 21:22:05 +0700

    Яндекс карта с меткой о текущем местоположении(должно быть включено местоположение в настройках приложения)

--- app/src/main/java/Krossbelling/geolocation/MainActivity.java
+++ app/src/main/java/Krossbelling/geolocation/MainActivity.java
@@ -1,14 +1,94 @@
 package Krossbelling.geolocation;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
+import android.graphics.PointF;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKit;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.layers.ObjectEvent;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.CompositeIcon;
+import com.yandex.mapkit.map.IconStyle;
+import com.yandex.mapkit.map.RotationType;
+import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.user_location.UserLocationLayer;
+import com.yandex.mapkit.user_location.UserLocationObjectListener;
+import com.yandex.mapkit.user_location.UserLocationView;
+import com.yandex.runtime.image.ImageProvider;
+
+
+public class MainActivity extends AppCompatActivity implements UserLocationObjectListener {
+    private MapView mapview;
+    private UserLocationLayer userLocationLayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
+
+        MapKitFactory.setApiKey("75db84e4-3f8a-471d-8542-95162df4a7d6");
+        MapKitFactory.initialize(this);
         setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        mapview = findViewById(R.id.mapview);
+        mapview.getMap().setRotateGesturesEnabled(false);
+        mapview.getMap().move(new CameraPosition(new Point(0, 0), 14, 0, 0));
+
+        MapKit mapKit = MapKitFactory.getInstance();
+        userLocationLayer = mapKit.createUserLocationLayer(mapview.getMapWindow());
+        userLocationLayer.setVisible(true);
+        userLocationLayer.setHeadingEnabled(true);
+
+        userLocationLayer.setObjectListener(this);
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        MapKitFactory.getInstance().onStop();
+        mapview.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapview.onStart();
+
+    }
+
+
+    @Override
+    public void onObjectAdded(UserLocationView userLocationView) {
+        userLocationLayer.setAnchor(
+                new PointF((float)(mapview.getWidth() * 0.5), (float)(mapview.getHeight() * 0.5)),
+                new PointF((float)(mapview.getWidth() * 0.5), (float)(mapview.getHeight() * 0.83)));
+
+        CompositeIcon pinIcon = userLocationView.getPin().useCompositeIcon();
+        pinIcon.setIcon(
+                "pin",
+                ImageProvider.fromResource(this, R.drawable.mapmarker),
+                new IconStyle().setAnchor(new PointF(0.5f, 0.5f))
+                        .setRotationType(RotationType.ROTATE)
+                        .setZIndex(1f)
+                        .setScale(0.5f)
+        );
+
+        userLocationView.getAccuracyCircle().setFillColor(Color.BLUE & 0x99ffffff);
+
+    }
+
+    @Override
+    public void onObjectRemoved(UserLocationView userLocationView) {
+
+    }
+
+    @Override
+    public void onObjectUpdated(UserLocationView userLocationView, ObjectEvent objectEvent) {
+
     }
 }
