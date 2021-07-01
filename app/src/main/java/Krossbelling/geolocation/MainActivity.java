7edbab3d707394e5b407f3975ebf5a572728b059
Krossbelling <Githubbalance@mail.ru>
2021-07-02 01:41:41 +0700

    Перемещение по координатам и установка метки

--- app/src/main/java/Krossbelling/geolocation/MainActivity.java
+++ app/src/main/java/Krossbelling/geolocation/MainActivity.java
@@ -3,11 +3,9 @@ package Krossbelling.geolocation;
 import android.graphics.Color;
 import android.graphics.PointF;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
+import android.view.View;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.yandex.mapkit.Animation;
 import com.yandex.mapkit.MapKit;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
@@ -23,14 +21,17 @@ import com.yandex.mapkit.user_location.UserLocationView;
 import com.yandex.runtime.image.ImageProvider;
 
 
+
 public class MainActivity extends AppCompatActivity implements UserLocationObjectListener {
     private MapView mapview;
     private UserLocationLayer userLocationLayer;
 
+    double latitude;
+    double longitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
 
         MapKitFactory.setApiKey("75db84e4-3f8a-471d-8542-95162df4a7d6");
         MapKitFactory.initialize(this);
@@ -61,13 +62,29 @@ public class MainActivity extends AppCompatActivity implements UserLocationObjec
         mapview.onStart();
 
     }
+    public void onClick(View view) {
 
+        EditText latitudeEdit = findViewById(R.id.editTextNumberDecimal);
+        latitude = Double.parseDouble(latitudeEdit.getText().toString());
+        EditText longitudeEdit = findViewById(R.id.editTextNumberDecimal2);
+        longitude = Double.parseDouble(longitudeEdit.getText().toString());
+
+        mapview.getMap().move(new CameraPosition(new Point(latitude, longitude), 14, 0, 0));
+        Point point = new Point(latitude, longitude);
+        // Чтобы очистить все прошлые метки, кроме метки местоположения телефона
+        // mapview.getMap().getMapObjects().clear();
+        mapview.getMap().getMapObjects().addPlacemark(point, ImageProvider.fromResource(this, R.drawable.mapmarker));
+
+
+    }
 
     @Override
     public void onObjectAdded(UserLocationView userLocationView) {
         userLocationLayer.setAnchor(
                 new PointF((float)(mapview.getWidth() * 0.5), (float)(mapview.getHeight() * 0.5)),
                 new PointF((float)(mapview.getWidth() * 0.5), (float)(mapview.getHeight() * 0.83)));
+
+
 
         CompositeIcon pinIcon = userLocationView.getPin().useCompositeIcon();
         pinIcon.setIcon(
@@ -78,6 +95,7 @@ public class MainActivity extends AppCompatActivity implements UserLocationObjec
                         .setZIndex(1f)
                         .setScale(0.5f)
         );
+
 
         userLocationView.getAccuracyCircle().setFillColor(Color.BLUE & 0x99ffffff);
 
